abb6641a724804eaf2835cdfb14591446ecb497f
Rodrigo Gonzalez <rodrigo@axented.com>
2017-12-14 10:40:12 -0600

    Change property name from private to public

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.2.1 (14/12/2017)
+
+- Change property name from private to public
--- src/themify-icons/themify.component.ts
+++ src/themify-icons/themify.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 
 export class ThemifyComponent implements OnInit {
-  @Input() private name: string;
+  @Input() public name: string;
   private _options: string[] = [];
 
   public ngOnInit() {
